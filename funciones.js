e2d9715e8bdc406432a75fdfcf691d84930272c6
Jaime de Greiff <alegreiff@gmail.com>
2023-02-25 12:34:26 -0500

    tabla creada

--- funciones.js
+++ funciones.js
@@ -6,3 +6,12 @@ function doGet() {
 function obtenerDatosHtml(archivo) {
   return HtmlService.createHtmlOutputFromFile(archivo).getContent();
 }
+
+function obtenerDatos() {
+  const idSheet = "1mRN1QamZSyagMdANkrb-fc_WQDh9v79_P97PxTOfdPI";
+  let hoja = SpreadsheetApp.openById(idSheet).getSheetByName("nombres");
+  let datos = hoja.getDataRange().getValues();
+  //datos = datos.filter((d) => d[0] > 0);
+  datos = datos.filter((d) => d[0] != "");
+  return datos;
+}
